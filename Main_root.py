f90bfa079fd7170ddd258b4c3317a06ffca5842d
TangShun1 <2838802308@qq.com>
2025-05-04 13:02:53 +0800

    ts

--- Main_root.py
+++ Main_root.py
@@ -24,12 +24,31 @@ def copy_text():
     messagebox.showinfo("提示", "复制文字")
 
 
-def guide():
-    messagebox.showinfo("提示", "使用指南")
+def transparency():
+    root1 = tk.Toplevel(root)
+    root1.title('设置窗口透明度')
+    root1.geometry('300x120')
+
+    label = tk.Label(root1, text='请选择窗口透明度（0-100）')
+    label.place(x=80, y=10)
+
+    #透明度进度条范围
+    scale = tk.Scale(root1, from_=0, to=100, orient='horizontal')#水平滑动
+    scale.set(0)
+    scale.place(x=100, y=30)
+
+    def change():
+        value = scale.get()#获取用户选择的透明度值
+        alpha = 1 - value / 100#透明度的范围，实际上是0-1
+        root.attributes('-alpha', alpha)#-alpha是设置主窗口透明度的属性
+        root1.destroy()
+
+    button = tk.Button(root1, text='确认', command=change)
+    button.place(x=135, y=85)
 
 
-def more():
-    messagebox.showinfo("提示", "更多功能111111111111")
+def size():
+    messagebox.showinfo("提示", "窗口大小")
 
 
 #按钮布置
@@ -38,8 +57,8 @@ buttons = [
     ("AI作答", ai_answer),
     ("AI对话", ai_chat),
     ("复制文字", copy_text),
-    ("使用指南", guide),
-    ("更多功能", more)
+    ("透明度", transparency),
+    ("窗口大小", size)
 ]
 for index, (text, command) in enumerate(buttons):
     row = index // 3
